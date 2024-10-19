114c46cdfdef14bc9b4127e8fd119f7c3390463e
Anna Tuhusova <bilachayka@mail.com>
2024-10-19 15:59:06 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,23 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  Object.keys(data).forEach((key) => {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (data[key] !== null && typeof data[key] === 'object') {
+      li.textContent = data[key];
+      createTree(li, data[key]);
+    } else {
+      li.textContent += `: ${data[key]}`;
+    }
+    ul.appendChild(li);
+  });
+
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
